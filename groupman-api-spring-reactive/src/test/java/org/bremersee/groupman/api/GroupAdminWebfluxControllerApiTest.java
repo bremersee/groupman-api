059c1e4e8b54ae2facd4f3ed3e2c7c18a1a4a5a5
Christian Bremer <bremerseegooglemail.com>
2020-01-02 22:22:26 +0100

    Renames reactive controllers and adds rest api annotation test.

--- groupman-api-spring-reactive/src/test/java/org/bremersee/groupman/api/GroupAdminWebfluxControllerApiTest.java
+++ groupman-api-spring-reactive/src/test/java/org/bremersee/groupman/api/GroupAdminWebfluxControllerApiTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import org.junit.jupiter.api.Test;
 
 /**
  * The group admin controller api test.
+ *
+ * @author Christian Bremer
  */
 class GroupAdminWebfluxControllerApiTest {
 
